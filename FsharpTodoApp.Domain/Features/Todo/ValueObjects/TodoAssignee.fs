f1fac310ca75f7267eae1e649c8c001c92f57c5a
V-nyang <l64906947+nftug@users.noreply.github.com>
2025-09-30 02:12:26 +0900

    refactor: Refactor assignment rules to assign reviewers and assignees

--- FsharpTodoApp.Domain/Features/Todo/ValueObjects/TodoAssignee.fs
+++ FsharpTodoApp.Domain/Features/Todo/ValueObjects/TodoAssignee.fs
@@ -10,12 +10,15 @@ module TodoAssignee =
     let value (TodoAssignee assignee) = assignee
 
     let (|AssignAllowed|AssignBlocked|) (actor, assignee) =
-        match value assignee with
-        | None -> AssignAllowed
-        | Some assignee when actor |> Actor.isAtLeast Manager || actor.UserInfo = assignee -> AssignAllowed
+        match actor, value assignee with
+        | _, None -> AssignAllowed
+        | a, _ when a |> Actor.isAtLeast Manager -> AssignAllowed
+        | a, Some assigneeInfo when a.UserInfo = assigneeInfo -> AssignAllowed
         | _ -> AssignBlocked
 
     let tryAssign ctx newAssignee =
         match ctx.Actor, TodoAssignee newAssignee with
         | AssignAllowed -> Ok(TodoAssignee newAssignee)
-        | AssignBlocked -> Validation.error "Only admin can assign other users."
+        | AssignBlocked -> Validation.error "Only manager and admin can assign other users."
+
+    let recreate assignee = TodoAssignee assignee
--- FsharpTodoApp.Domain/Features/Todo/ValueObjects/TodoReviewer.fs
+++ FsharpTodoApp.Domain/Features/Todo/ValueObjects/TodoReviewer.fs
@@ -10,14 +10,15 @@ module TodoReviewer =
     let value (TodoReviewer reviewer) = reviewer
 
     let (|ReviewAllowed|ReviewBlocked|) (actor, reviewer) =
-        match value reviewer with
-        | None -> ReviewAllowed
-        | Some reviewerInfo when actor |> Actor.isAtLeast Manager || actor.UserInfo = reviewerInfo -> ReviewAllowed
+        match actor, value reviewer with
+        | _, None -> ReviewAllowed
+        | a, _ when a |> Actor.isAtLeast Manager -> ReviewAllowed
+        | a, Some reviewerInfo when a.UserInfo = reviewerInfo -> ReviewAllowed
         | _ -> ReviewBlocked
 
     let tryAssign ctx newReviewer =
         match ctx.Actor, TodoReviewer newReviewer with
         | ReviewAllowed -> Ok(TodoReviewer newReviewer)
-        | ReviewBlocked -> Validation.error "Only admin can assign other users."
+        | ReviewBlocked -> Validation.error "Only manager and admin can assign other users."
 
     let recreate reviewer = TodoReviewer reviewer
